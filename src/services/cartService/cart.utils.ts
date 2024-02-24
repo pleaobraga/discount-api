701e9509ae370f8d8019ce7cff4fea31e5fcb83a
Pedro Braga <pleao.braga@gmail.com>
2024-02-24 10:33:52 -0300

    feat: create error object

--- src/services/cartService/cart.utils.ts
+++ src/services/cartService/cart.utils.ts
@@ -1,4 +1,4 @@
-import { AppError } from '../../errors/AppErrors'
+import { AppError } from '../../errors/appErrors'
 import { parseFixedNumber } from '../../helper/parse'
 import {
   type DiscountProductsCart,
